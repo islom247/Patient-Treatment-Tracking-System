8b8ce3e9ff369e99dd1f0d2c05dcb3818618a678
islomiddin <islomiddin.sodiqov@gmail.com>
2019-01-01 22:18:04 +0300

    added more table(s)

--- Database/Main.java
+++ Database/Main.java
@@ -21,6 +21,7 @@ public class Main {
         statement.executeUpdate("DROP TABLE IF EXISTS does");
         statement.executeUpdate("DROP TABLE IF EXISTS asks_for");
         statement.executeUpdate("DROP TABLE IF EXISTS prescribes");
+        statement.executeUpdate("DROP TABLE IF EXISTS diagnose");
         statement.executeUpdate("DROP TABLE IF EXISTS appointment");
         statement.executeUpdate("DROP TABLE IF EXISTS pharmacist");
         statement.executeUpdate("DROP TABLE IF EXISTS patient");
@@ -33,7 +34,6 @@ public class Main {
         statement.executeUpdate("DROP TABLE IF EXISTS disease");
         statement.executeUpdate("DROP TABLE IF EXISTS prescription");
         statement.executeUpdate("DROP TABLE IF EXISTS drug");
-        statement.executeUpdate("DROP TABLE IF EXISTS diagnose");
         statement.executeUpdate("DROP TABLE IF EXISTS results");
         statement.executeUpdate("DROP TABLE IF EXISTS component");
         statement.executeUpdate("DROP TABLE IF EXISTS consists");
@@ -171,6 +171,13 @@ public class Main {
                                 "FOREIGN KEY (trans_id) REFERENCES transaction(trans_id)," +
                                 "FOREIGN KEY (pharmacist_username) REFERENCES pharmacist(username)," +
                                 "FOREIGN KEY (pres_id) REFERENCES prescription(prescription_id))" +
+                                "ENGINE=INNODB");
+
+        statement.executeUpdate("CREATE TABLE diagnose(app_id int NOT NULL," +
+                                "dis_id int NOT NULL," +
+                                "PRIMARY KEY(app_id, dis_id)," +
+                                "FOREIGN KEY (app_id) REFERENCES appointment(app_id)," +
+                                "FOREIGN KEY (dis_id) REFERENCES disease(dis_id))" +
                                 "ENGINE=INNODB");
     }
 }
