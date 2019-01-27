f3cf57c07ce577e843d754c0706250245e921406
islom247 <islomiddin.sodiqov@gmail.com>
2019-01-27 22:39:34 +0300

    few bugs removed

---
+++ Database/login.php
@@ -0,0 +1,102 @@
+<?php
+	include("config.php");
+	session_start();
+	if ($con->connect_error) {
+		die("Connection failed: " . $con->connect_error);
+	}
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		//checking whether the user input is empty
+		$username = mysqli_real_escape_string($con, $_POST['uname']);
+		$password = mysqli_real_escape_string($con, $_POST['psw']);
+		$query = "SELECT * FROM user WHERE username = ? and password = ?";
+		
+		//preparing a statement
+		if ($stmt = $con->prepare($query)) {
+			//binding parameters into prepared statement
+			$stmt->bind_param('ss', $username, $password);
+			
+			//executing the query(statement)
+			$stmt->execute();
+			
+			//getting the result
+			$result = $stmt->get_result();
+			
+			//number of rows in result set
+			$count = $result->num_rows;
+			
+			//if count is 1 the user entered correct data
+			if ($count == 1) {
+				
+				//First we check if the user is doctor or not
+				$doc_query = "SELECT * FROM doctor WHERE username = ?";
+				if ($doc_stmt = $con->prepare($doc_query)) {
+					$doc_stmt->bind_param('s', $username);
+					$doc_stmt->execute();
+					$doc_result = $doc_stmt->get_result();
+					$doc_count = $doc_result->num_rows;
+					
+					//if the user is doctor we redirect to doctor profile page
+					if ($doc_count == 1) {
+						$_SESSION['username'] = $username;
+						$_SESSION['password'] = $password;
+						$doctor_checked = true;
+						header("location: doctor.php");
+						exit();
+					}
+				}
+				
+				//now we check is the user is a patient
+				$pat_query = "SELECT * FROM patient WHERE username = ?";
+				if ($pat_stmt = $con->prepare($pat_query)) {
+					$pat_stmt->bind_param('s', $username);
+					$pat_stmt->execute();
+					$pat_result = $pat_stmt->get_result();
+					$pat_count = $pat_result->num_rows;
+					
+					//if the user is patient we redirect to patient profile page
+					if ($pat_count == 1) {
+						$_SESSION['username'] = $username;
+						$_SESSION['password'] = $password;
+						header("location: patient.php");
+						exit();
+					}
+				}
+			} else {
+				//warning the user about the wrong data input
+				$field_error = "Wrong username or password!";
+			}
+		}
+	}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="index_css.css">
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <div class = "top">
+        <img src="logo.png" class = "logo">
+        <label id = "title"> Patient Medical Treatment Tracking System</label>
+        <a href="signup_first.php"><button type = "submit" class = "top_but" id = "sign" >Sign up</button></a>
+        <button type = "submit" class = "top_but" id = "log" >Log in</button>
+    </div>
+
+    <div class="container">
+		<form action="" method="post">
+        <label><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="uname" required></input>
+
+        <label><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required></input>
+
+        <button type="submit" class = "user_log">Login</button>
+        <label>
+            <input type="checkbox" class = "checked" name = "remember"> Remember me</input>
+        </label>
+		<div style="color:red; font-size:20px"><br><?php echo $field_error?></div>
+		</form>
+    </div>
+</body>
+</html>
---
+++ Database/patient.php
@@ -0,0 +1,109 @@
+<?php 
+	include("config.php");
+	session_start();
+	if ($con->connect_error) {
+		die("Connection failed: " . $con->connect_error);
+	}
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		$query = "INSERT INTO address (country, city, street, apartment, apartment_num) VALUES(?,?,?,?,?)";
+		if ($stmt = $con->prepare($query)) {
+			$country = $_POST["country"];
+			$city = $_POST["city"];
+			$street = $_POST["street"];
+			$apt_name = $_POST["apt_name"];
+			$apt_no = $_POST["apt_no"];
+			$stmt->bind_param('ssssi', $country, $city, $street, $apt_name, $apt_no);
+			$stmt->execute();
+			$add_id = mysqli_insert_id($con);
+		}
+		$query = "INSERT INTO user (username, image, phone, name, password, birthday, gender, add_id) VALUES(?,?,?,?,?,?,?,?)";
+		if ($stmt = $con->prepare($query)) {
+			$username = $_POST["uname"];
+			$image = "image.png";
+			$phone = $_POST["phone"];
+			$name = $_POST["name"];
+			$password = $_POST["psw"];
+			$rawdate = htmlentities($_POST["date"]);
+			$birthday = date('d.m.Y', strtotime($rawdate));
+			$gender = $_POST["gender"];
+			$stmt->bind_param("sssssssi", $username, $image, $phone, $name, $password, $birthday, $gender, $add_id);
+			$stmt->execute();
+		}
+		$query = "INSERT INTO patient (username) VALUES (?)";
+		if ($stmt = $con->prepare($query)) {
+			$stmt->bind_param("s", $username);
+			$stmt->execute();
+		}
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="patient_css.css">
+    <meta charset="UTF-8">
+    <title>Profile</title>
+</head>
+<body>
+    <div class = "top">
+        <img src="logo.png" class = "logo">
+        <label id = "title"> Patient Medical Treatment Tracking System</label>
+        <a href="logout.php"><button type = "submit" class = "top_but" id = "right_1" name = "logout" >Log out</button></a>
+        <button type = "submit" class = "top_but" id = "right_2" name = "switch_doc">Doctor</button>
+        <button type = "submit" class = "top_but" id = "right_3" name = "switch_pharm">Pharmacist</button>
+    </div>
+
+    <div class = "menu">
+		<button type = "submit" class = "menu_but" id = "top_1" name = "profile" >Profile</button>
+		<button type = "submit" class = "menu_but" id = "top_2" name = "app" >Appointments</button>
+		<button type = "submit" class = "menu_but" id = "top_3" name = "doctors" >Doctors</button>
+		<button type = "submit" class = "menu_but" id = "top_4" name = "doctors" >Transactions</button>
+		<button type = "submit" class = "menu_but" id = "top_5" name = "set" >Settings</button>
+		<button type = "submit" class = "menu_but" id = "top_6" name = "developer" >Developers</button>
+    </div>
+
+<div class="container">
+	<?php
+		$query = "SELECT phone, name, birthday, gender, add_id FROM user where username = ?";
+		$add_id = 0;
+		if ($stmt = $con->prepare($query)) {
+			if ($stmt && $stmt->bind_param('s', $_SESSION['username']) && $stmt -> execute() && $stmt -> store_result() && $stmt -> bind_result($phone, $name, $birthday, $gender, $cur_add_id)) {				
+				while ($stmt -> fetch()) {
+					if ($add_id==0) {
+						$add_id = $cur_add_id;
+						break;
+					}
+				}
+				$birthday = htmlentities($birthday);
+				$birthday = date('d.m.Y', strtotime($birthday));
+			}
+		}
+		
+		$query = "SELECT * FROM address where add_id = ?";
+		$flag = false;
+		if ($stmt = $con->prepare($query)) {
+			if ($stmt && $stmt->bind_param('i', $add_id) && $stmt -> execute() && $stmt -> store_result() && $stmt -> bind_result($cur_id, $country, $city, $street, $apt_name, $apt_no)) {
+				//echo $_SESSION['username'];
+				while ($stmt -> fetch()) {
+					if (!$flag) {
+						$flag = true;
+						break;
+					}
+				}
+			}
+		}
+	?>
+	<img src="img_avatar2.jpg" alt="Avatar" class="avatar"><br>
+	<div class="info">
+		<label><b>Name:     <?php echo $name;?></b></label><br></div>
+	<div class="info"> <label><b>Address:     <?php echo '  '.$country.',  '.$city.',  '.$street.' Street,  '.$apt_name.' / '.$apt_no;?></b></label><br></div>
+	<div class="info">
+		<label><b>Date of birth:     <?php echo '  '.$birthday;?></b></label><br></div>
+	<div class="info">
+		<label><b>Gender:     <?php echo '  '.$gender;?></b></label><br></div>
+	<div class="info">
+		<label><b>Phone:     <?php echo '  '.$phone;?></b></label><br></div>
+	<button type = "submit" class = "user_log" name = "edit">Edit profile</button>
+</div>
+</body>
+</html>
